a56f2572930fc4402635a83d109a2934966fd895
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2018-03-28 13:10:02 +0200

    frontend simon dinges
    ik  heb heel hard me best gedaan

---
+++ frontendSimon/php/index.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="../css/normalize.css" />
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+</head>
+<body>
+<div class="navbar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li style="float: right"><a href="reserveer.php">Boeken</a></li>
+    </ul>
+</div>
+<div class="container">
+    <div id="startDiv" class="col s2">
+
+        <h2>Start</h2>
+    </div>
+</div>
+
+
+
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</body>
+</html>
